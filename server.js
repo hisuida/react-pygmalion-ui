bbc156d061ac06712143378a63d3648397c0bf8c
Jeane Hong <jeane@degreed.com>
2021-10-12 17:24:36 -0500

    reorder gets

--- server.js
+++ server.js
@@ -11,21 +11,12 @@ app.use(express.json());
 // declare react files in build as static
 app.use(express.static(path.join(__dirname, "build")));
 
-// serve index.html from the build folder
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 //create connection to database
 const db = mysql.createPool({
   host: process.env.DB_HOST, //localhost
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD, //password
   database: process.env.DB,
-});
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("App is listening on port " + listener.address().port);
 });
 
 app.get("/order/:id", (req, res) => {
@@ -40,4 +31,13 @@ app.get("/order/:id", (req, res) => {
       }
     }
   );
+});
+
+// serve index.html from the build folder
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log("App is listening on port " + listener.address().port);
 });
