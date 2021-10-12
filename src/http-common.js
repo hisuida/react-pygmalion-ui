b1e6f40feb26a2f7400a515a6a3498d4748350db
Jeane Hong <jeane@degreed.com>
2021-10-12 17:13:45 -0500

    change some settings

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://pygmalion-serial.herokuapp.com/",
+  baseURL:
+    process.env.NODE_ENV === "dev"
+      ? `http://localhost:3306`
+      : "https://pygmalion-serial.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
   },
